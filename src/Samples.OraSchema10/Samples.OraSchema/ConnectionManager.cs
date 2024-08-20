5f984ffe9630da33e33a19473ebf96d863fa1095
Martin A. Ortega <lynxestudio@outlook.com>
2024-08-20 01:24:16 -0600

    Adds source code to the project

---
+++ src/Samples.OraSchema10/Samples.OraSchema/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using Oracle.DataAccess.Client;
+using System.Data;
+
+namespace Samples.OraSchema
+{
+    internal sealed class ConnectionManager
+    {
+        static OracleConnection connection = null;
+        internal static OracleConnection OpenAndGetConnection(string connectionString)
+        {
+            connection = new OracleConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+        internal static void CloseConnection()
+        {
+            if (connection != null)
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                    connection = null;
+                }
+            }
+        }
+    }
+}
